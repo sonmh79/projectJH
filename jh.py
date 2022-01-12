aa018d127986463aa994f2a53bfe28b58e860e94
Minhyeok <sonmh79@naver.com>
2022-01-12 15:28:07 +0900

    Version 2.0

--- jh.py
+++ jh.py
@@ -8,7 +8,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import uic
 from selenium import webdriver
-#from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -27,21 +26,20 @@ class WindowClass(QMainWindow, form_class) :
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
         # Fixed Variable
-        self.AE10 = {"MOGENS MAERSK": "1RM", "MARSEILLE MAERSK":"Y29", "MARIE MAERSK" : "1JM", "MAJESTIC MAERSK":"1HM", "MADISON MAERSK":"1KM","MATHILDE MAERSK":"2BM"}
-        self.AE05 = {"MAASTRICHT MAERSK":"Y34","MURCIA MAERSK":"Y31","METTE MAERSK":"1ZM","MUNICH MAERSK":"Y25","MERETE MAERSK":"1QM","MADRID MAERSK":"Y24"}
+        self.AE10 = {"MOGENS MAERSK": "1RM", "MARSEILLE MAERSK":"Y29", "MARIE MAERSK" : "1JM", "MAJESTIC MAERSK":"1HM", "MADISON MAERSK":"1KM","MATHILDE MAERSK":"2BM", "MANCHESTER MAERSK":"Y30","MARY MAERSK":"1IM","MARIT MAERSK":"2AM","MAGLEBY MAERSK":"1LM","MAYVIEW MAERSK":"1PM"}
+        self.AE05 = {"MAASTRICHT MAERSK":"Y34","MURCIA MAERSK":"Y31","METTE MAERSK":"1ZM","MUNICH MAERSK":"Y25","MERETE MAERSK":"1QM","MADRID MAERSK":"Y24","MARGRETHE MAERSK":"1XM","MILAN MAERSK":"Y27","MSC RIFAYA":"F6M"}
         self.vessel_codes = dict(self.AE10, **self.AE05)
         self.target_ports = ["Gdansk", "Bremerhaven"]
         self.months = {"Jan":1, "Feb":2, "Mar":3, "Apr":4, "May":5, "Jun":6, "Jul":7, "Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12 }
-        self.filename = "schedule.xlsx"
+        self.filename = "test.xlsx"
         self.wb = openpyxl.load_workbook(filename=self.filename, read_only=False, data_only=True)
         self.ws = self.wb[self.wb.sheetnames[0]]
-
-
 
         # Vessel Variables ( Updated from Clicked Cell )
         self.selected_vessel = "MARIE MAERSK"
         self.vessel_w = ""
 
+        # Date Variables
         self.date = QDate.currentDate()
         self.cur_date = self.date.toString("yyyy-MM-dd")
         self.dateEdit.setDate(self.date)
@@ -63,24 +61,24 @@ class WindowClass(QMainWindow, form_class) :
         except:
             pass
 
-        #self.crawl()
         self.initTable()
 
         # Connect Functions with Widgets
         self.btn_update.clicked.connect(self.crawl)
         self.dateEdit.dateChanged.connect(self.dateChanged)
-        self.btn_start.clicked.connect(self.setTimer)
-        self.btn_stop.clicked.connect(self.stopTimer)
-        self.cmb_interval.currentIndexChanged.connect(self.changeInterval)
         self.btn_refresh.clicked.connect(self.initTable)
         self.btn_updateValue.clicked.connect(self.updateValue)
+        # self.btn_start.clicked.connect(self.setTimer)
+        # self.btn_stop.clicked.connect(self.stopTimer)
+        # self.cmb_interval.currentIndexChanged.connect(self.changeInterval)
 
     def updateValue(self):
 
         """Update Value Modified by User"""
 
-
-        target = chr(int(self.c) + 65) + str(self.r + 2) # (0,0) -> A1, Z열까지만 가능
+        excel_row = self.r + 2
+        excel_col = self.c + 1
+        target = get_column_letter(excel_col) + str(excel_row)
         newValue = self.valueEdit.text()
         self.ws[target] = newValue
         self.wb.save(self.filename)
@@ -114,13 +112,13 @@ class WindowClass(QMainWindow, form_class) :
         layout.addWidget(e)
 
         self.r, self.c, self.valueEdit = r, c, e
-        print(self.r,self.c)
+
 
     def setLabel1(self, row, column):
 
-        """Change Index Table1 to Original DataFrame"""
+        """Change Table1 Index to Dataframe Index"""
 
-        r,c = row+self.visible_rows[0][0]-2,column+1
+        r,c = row + self.visible_rows[0][0]-2, column + 1
         self.showClickedLabel(r,c,t1=True)
 
         self.is_AE10 = True
@@ -128,10 +126,9 @@ class WindowClass(QMainWindow, form_class) :
 
     def setLabel2(self, row, column):
 
-        """Change Index Table2 to Original DataFrame"""
+        """Change Table2 Index to Dataframe Index"""
 
         r, c = row + self.visible_rows[1][0]-2, column + 1
-        print(self.df.iloc[r, c])
         self.showClickedLabel(r, c, t1 = False)
 
         self.is_AE10 = False
@@ -141,10 +138,11 @@ class WindowClass(QMainWindow, form_class) :
 
         """Initialize Table"""
 
+        df = pd.read_excel(self.filename)
         ws = self.ws
         visible_rows = [[],[]]
         flag = True
-        for i in range(4,35):
+        for i in range(4,len( df)+1):
             if tuple(ws.row_dimensions[i])[0][0] != "hidden":
                 cell_value = ws.cell(row=i,column=2).value
                 if cell_value != None:
@@ -155,12 +153,11 @@ class WindowClass(QMainWindow, form_class) :
                             visible_rows[1].append(i)
                     else:
                         flag = False
-        print(visible_rows)
+
         self.visible_rows = visible_rows
 
         table1 = self.table1
         table2 = self.table2
-        df = pd.read_excel(self.filename)
 
         df1 = df.iloc[visible_rows[0][0]-2:visible_rows[0][-1]+1-2,1:]
         df2 = df.iloc[visible_rows[1][0]-2:,1:8]
@@ -187,37 +184,37 @@ class WindowClass(QMainWindow, form_class) :
         table2.cellClicked.connect(self.setLabel2)
 
 
-    def changeInterval(self):
-
-        """Set Auto-Crawling Interval"""
-
-        text = self.cmb_interval.currentText()
-        if text == "1분":
-            self.interval = 1000 * 60
-        elif text == "10분":
-            self.interval = 1000 * 60 * 10
-        elif text == "30분":
-            self.interval = 1000 * 60 * 30
-        elif text == "1시간":
-            self.interval = 1000 * 60 * 60
-        elif text == "10시간":
-            self.interval = 1000 * 60 * 60 * 10
-
-    def stopTimer(self):
-
-        """Stop Timer"""
-
-        self.timerVar.stop()
-        self.txt_status.setText("OFF")
-
-    def setTimer(self):
-
-        """Set Timer"""
-
-        self.timerVar.setInterval(self.interval)
-        self.timerVar.timeout.connect(self.crawl)
-        self.timerVar.start()
-        self.txt_status.setText("ON")
+    # def changeInterval(self):
+    #
+    #     """Set Auto-Crawling Interval"""
+    #
+    #     text = self.cmb_interval.currentText()
+    #     if text == "1분":
+    #         self.interval = 1000 * 60
+    #     elif text == "10분":
+    #         self.interval = 1000 * 60 * 10
+    #     elif text == "30분":
+    #         self.interval = 1000 * 60 * 30
+    #     elif text == "1시간":
+    #         self.interval = 1000 * 60 * 60
+    #     elif text == "10시간":
+    #         self.interval = 1000 * 60 * 60 * 10
+    #
+    # def stopTimer(self):
+    #
+    #     """Stop Timer"""
+    #
+    #     self.timerVar.stop()
+    #     self.txt_status.setText("OFF")
+    #
+    # def setTimer(self):
+    #
+    #     """Set Timer"""
+    #
+    #     self.timerVar.setInterval(self.interval)
+    #     self.timerVar.timeout.connect(self.crawl)
+    #     self.timerVar.start()
+    #     self.txt_status.setText("ON")
 
     def dateChanged(self):
 
@@ -234,16 +231,34 @@ class WindowClass(QMainWindow, form_class) :
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         return reply
 
+    def message_warning(self,title,text):
+
+        """ Qmessagebox - Question """
+
+        reply = QMessageBox.warning(self, title, text,
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        return reply
+
     def crawl(self):
 
         """Start Crawling"""
+
+        if self.selected_vessel not in self.vessel_codes:
+            return self.message_warning("","Please Click the Vessel First")
+
         qdate = self.date
         driver = self.driver
 
         flag = True
         search_BGB = False
         BGB_result = []
+        crawl_cnt = 0
         while flag:
+            crawl_cnt += 1
+
+            if crawl_cnt >= 20:
+                return self.message_warning("","Cannot Find Data..")
+
             date = qdate.toString("yyyy-MM-dd")
             self.url = f"http://www.maersk.com/schedules/vesselSchedules?vesselCode={self.vessel_codes[self.selected_vessel]}&fromDate={date}"
             driver.get(self.url)
@@ -281,8 +296,8 @@ class WindowClass(QMainWindow, form_class) :
                 BGB_result = [("Bremerhaven", "Omit")] + BGB_result
             else:
                 BGB_result += [("Bremerhaven", "Not Yet")]
-        text = ""
-        print(BGB_result)
+        text = self.selected_vessel + self.vessel_w + "\n"
+
         for result in BGB_result:
             text += f"{result[0]} - {result[1]} \n"
 
